60ba145407ac57501f18c58d153fe7a488ff48c7
HazilTheNut <hurin615@gmail.com>
2018-06-06 17:04:57 -0500

    retry remove .idea stuff

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -187,7 +187,7 @@ public class Player extends CombatEntity implements MouseInputReceiver, KeyListe
         super.receiveDamage(amount);
         hud.updateHUD();
         if (pathingThread != null && pathingThread.isAlive()) terminatePathing = true;
-        if (getHealth() <= 0) gi.getGameMaster().exitGame();
+        if (getHealth() < 1) gi.getGameMaster().exitGame();
     }
 
     @Override
