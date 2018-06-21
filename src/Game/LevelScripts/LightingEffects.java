9feddccf38ee1668463a968eae69c8450859c8cc
HazilTheNut <hurin615@gmail.com>
2018-06-20 21:24:55 -0500

    Added smoothing filter for LightingEffects

--- src/Game/LevelScripts/LightingEffects.java
+++ src/Game/LevelScripts/LightingEffects.java
@@ -124,8 +124,6 @@ public class LightingEffects extends LevelScript {
         Layer tempLayer = new Layer(shadingLayer.getCols(), shadingLayer.getRows(), "shandingtemp", 0, 0, 0);
         for (int col = 0; col < level.getBackdrop().getCols(); col++) { //Iterate over every part of the level. //TODO: Clip range to just the screen.
             for (int row = 0; row < level.getBackdrop().getRows(); row++) {
-                lightMap = new double[level.getBackdrop().getCols()][level.getBackdrop().getRows()];
-                for (LightNode node : lightNodes) lightMap[col][row] += node.lightMap[col][row];
                 double lightness = lightMap[col][row];
                 double warmLightingCutoff = 4;
                 if (lightness < 1){ //Draw the cold colors if it is dim.
@@ -173,15 +171,61 @@ public class LightingEffects extends LevelScript {
      * Effectively calculates the lighting values of all the LightNodes
      */
     private void compileLightmaps(){
+        //Do raycasts
         for (LightNode lightNode : lightNodes){
             //dTheta is multiplied by the Math.min(...) function in order to increase its accuracy for low-luminance LightNodes, which can appear kinda grainy.
             double dTheta = Math.atan(Math.sqrt(lightingCutoff / lightNode.luminance)) * Math.min(lightNode.luminance, 0.65);
             for (double angle = 0; angle < Math.PI * 2; angle += dTheta) {
-                performRaycast(lightNode, angle);
+                performRaycast(lightNode, new Coordinate(lightNode.getX(), lightNode.getY()), angle);
             }
             lightNode.assignLightMapValue(lightNode.x, lightNode.y, lightNode.luminance);
         }
+        //Assemble the lightmaps
+        lightMap = new double[level.getBackdrop().getCols()][level.getBackdrop().getRows()];
+        for (int col = 0; col < level.getBackdrop().getCols(); col++) { //Iterate over every part of the level. //TODO: Clip range to just the screen.
+            for (int row = 0; row < level.getBackdrop().getRows(); row++) {
+                for (LightNode node : lightNodes) lightMap[col][row] += node.lightMap[col][row];
+            }
+        }
+        //Apply smoothing
+        //smoothLightMap();
     }
+
+    private void smoothLightMap(){
+        double[][] smoothedMap = new double[level.getBackdrop().getCols()][level.getBackdrop().getRows()];
+        for (int col = 0; col < smoothedMap.length; col++) { //Iterate over every part of the level. //TODO: Clip range to just the screen.
+            for (int row = 0; row < smoothedMap[0].length; row++) {
+                int numPoints = 0;
+                double total = 0;
+                if (getLightMapAmount(new Coordinate(col - 1, row)) >= 0){
+                    total += getLightMapAmount(new Coordinate(col - 1, row));
+                    numPoints++;
+                }
+                if (getLightMapAmount(new Coordinate(col, row + 1)) >= 0){
+                    total += getLightMapAmount(new Coordinate(col, row + 1));
+                    numPoints++;
+                }
+                if (getLightMapAmount(new Coordinate(col, row - 1)) >= 0){
+                    total += getLightMapAmount(new Coordinate(col, row - 1));
+                    numPoints++;
+                }
+                if (getLightMapAmount(new Coordinate(col + 1, row)) >= 0){
+                    total += getLightMapAmount(new Coordinate(col + 1, row));
+                    numPoints++;
+                }
+                smoothedMap[col][row] = (total + (lightMap[col][row] * numPoints)) / (numPoints * 2);
+            }
+        }
+        lightMap = smoothedMap;
+    }
+
+    private double getLightMapAmount(Coordinate pos){
+        if (level.getBackdrop().isLayerLocInvalid(pos))
+            return -1;
+        return lightMap[pos.getX()][pos.getY()];
+    }
+
+
 
     /**
      * Performs a raycast until it hits a wall, defining lighting values for all the points in between.
@@ -189,17 +233,18 @@ public class LightingEffects extends LevelScript {
      * @param node The LightNode to raycast from
      * @param angle The angle the raycast is pointed
      */
-    private void performRaycast(LightNode node, double angle){
+    private void performRaycast(LightNode node, Coordinate startPos, double angle){
         double xPerCycle = 0.9 * Math.cos(angle);
         double yPerCycle = 0.9 * Math.sin(angle);
-        float x = (float)node.getX();
-        float y = (float)node.getY();
+        float x = (float)startPos.getX();
+        float y = (float)startPos.getY();
         double distPerCycle = Math.sqrt(Math.pow(xPerCycle, 2) + Math.pow(yPerCycle, 2));
         int numberCycles = (int)(Math.ceil(Math.sqrt(node.luminance / lightingCutoff)) / distPerCycle);
-        for (int dist = 0; dist < numberCycles; dist++) {
+        for (int i = 0; i < numberCycles; i++) {
             x += xPerCycle;
             y += yPerCycle;
             Coordinate toCheck = new Coordinate(Math.round(x), Math.round(y));
+            double dist = Math.sqrt(Math.pow(node.x - x, 2) + Math.pow(node.y - y, 2));
             double lightness = node.luminance / Math.max(Math.pow(dist, 2), 1);
             node.assignLightMapValue(toCheck.getX(), toCheck.getY(), lightness);
             if (level.getTileAt(toCheck).hasTag(TagRegistry.TILE_WALL)){
