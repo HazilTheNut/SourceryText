a2977d5c32f77561dbd9a693245bce8fa9d1317f
HazilTheNut <hurin615@gmail.com>
2018-06-29 11:37:29 -0500

    Created room covers, can now conceal large secrets.

--- src/Game/Entities/Door.java
+++ src/Game/Entities/Door.java
@@ -1,7 +1,15 @@
 package Game.Entities;
 
+import Data.Coordinate;
+import Data.EntityArg;
+import Data.EntityStruct;
 import Data.SerializationVersion;
+import Engine.LayerManager;
+import Game.Entities.PuzzleElements.Powerable;
+import Game.GameInstance;
 import Game.Player;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jared on 5/13/2018.
@@ -11,13 +19,36 @@ public class Door extends Entity {
     /**
      * Door:
      *
-     * The extremely-complicated and nearly impossible-to-read superclass that defines the subtle behavior of a single-use door.
+     * An Entity that acts as a solid wall until interacted with. Additionally, it can power other elements upon opening.
      */
 
     private static final long serialVersionUID = SerializationVersion.SERIALIZATION_VERSION;
 
+    private ArrayList<Coordinate> powerToLocs;
+
+    @Override
+    public ArrayList<EntityArg> generateArgs() {
+        ArrayList<EntityArg> args = super.generateArgs();
+        args.add(new EntityArg("powerTo","[0,0],[0,0],...."));
+        return args;
+    }
+
+    @Override
+    public void initialize(Coordinate pos, LayerManager lm, EntityStruct entityStruct, GameInstance gameInstance) {
+        super.initialize(pos, lm, entityStruct, gameInstance);
+        powerToLocs = readCoordListArg(searchForArg(entityStruct.getArgs(), "powerTo"));
+    }
+
     @Override
     public void onInteract(Player player) {
+        for (Coordinate loc : powerToLocs){
+            for (Entity e : gi.getCurrentLevel().getEntitiesAt(loc)){
+                if (e instanceof Powerable) {
+                    Powerable powerable = (Powerable) e;
+                    powerable.onPowerOn();
+                }
+            }
+        }
         selfDestruct();
     }
 }
---
+++ src/Game/Entities/PuzzleElements/RoomCover.java
@@ -0,0 +1,110 @@
+package Game.Entities.PuzzleElements;
+
+import Data.*;
+import Engine.Layer;
+import Engine.LayerManager;
+import Engine.SpecialText;
+import Game.Entities.Entity;
+import Game.GameInstance;
+import Game.Registries.TagRegistry;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class RoomCover extends Entity implements Powerable {
+
+    private static final long serialVersionUID = SerializationVersion.SERIALIZATION_VERSION;
+
+    private Layer coverLayer;
+    private boolean includeWalls;
+
+    @Override
+    public ArrayList<EntityArg> generateArgs() {
+        ArrayList<EntityArg> args = super.generateArgs();
+        args.add(new EntityArg("bounds","[0,0]-[0,0]"));
+        args.add(new EntityArg("includeWalls","true"));
+        return args;
+    }
+
+    @Override
+    public boolean isSolid() {
+        return false;
+    }
+
+    @Override
+    public void initialize(Coordinate pos, LayerManager lm, EntityStruct entityStruct, GameInstance gameInstance) {
+        super.initialize(pos, lm, entityStruct, gameInstance);
+        ArrayList<Coordinate> bounds = readCoordListArg(searchForArg(entityStruct.getArgs(), "bounds"));
+        if (bounds.size() >= 2){
+            buildCoverLayer(bounds.get(0), bounds.get(1));
+            coverLayer.setVisible(true);
+            getSprite().editLayer(0, 0, null);
+        }
+        includeWalls = readBoolArg(searchForArg(entityStruct.getArgs(), "includeWalls"), true);
+    }
+
+    private void buildCoverLayer(Coordinate bound1, Coordinate bound2){
+        int left   = Math.min(bound1.getX(), bound2.getX());
+        int top    = Math.min(bound1.getY(), bound2.getY());
+        int right  = Math.max(bound1.getX(), bound2.getX());
+        int bottom = Math.max(bound1.getY(), bound2.getY());
+        coverLayer = new Layer(right - left + 1, bottom - top + 1, "cover" + getUniqueID(), left, top, LayerImportances.VFX-1);
+    }
+
+    private void createCover(){
+        ArrayList<Coordinate> locs = new ArrayList<>();
+        locs.add(getLocation());
+        for (int i = 0; i < locs.size();) {
+            Coordinate loc = locs.get(i);
+            attemptPoint(loc, loc.add(new Coordinate(1, 0)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(1, 1)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(0, 1)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(-1, 1)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(-1, 0)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(-1, -1)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(0, -1)), locs);
+            attemptPoint(loc, loc.add(new Coordinate(1, -1)), locs);
+            i++;
+        }
+        coverLayer.clearLayer();
+        for (Coordinate loc : locs){
+            coverLayer.editLayer(loc.subtract(coverLayer.getPos()), new SpecialText(' ', Color.WHITE, Color.BLACK));
+        }
+    }
+
+    private void attemptPoint(Coordinate origin, Coordinate attempt, ArrayList<Coordinate> locs){
+        boolean isSpace = (includeWalls && gi.isSpaceAvailable(origin, TagRegistry.TILE_WALL)) || (!includeWalls && gi.isSpaceAvailable(attempt, TagRegistry.TILE_WALL));
+        boolean alreadyThere = !locs.contains(attempt);
+        boolean isBounded = !coverLayer.isLayerLocInvalid(attempt.subtract(coverLayer.getPos()));
+        if (isSpace && alreadyThere && isBounded){
+            locs.add(attempt);
+        }
+    }
+
+    @Override
+    public void onLevelEnter() {
+        super.onLevelEnter();
+        if (coverLayer != null) {
+            createCover();
+            gi.getLayerManager().addLayer(coverLayer);
+        }
+    }
+
+    @Override
+    public void onLevelExit() {
+        super.onLevelExit();
+        if (coverLayer != null) {
+            gi.getLayerManager().removeLayer(coverLayer);
+        }
+    }
+
+    @Override
+    public void onPowerOff() {
+        coverLayer.setVisible(true);
+    }
+
+    @Override
+    public void onPowerOn() {
+        coverLayer.setVisible(false);
+    }
+}
