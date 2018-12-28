fb5e2f732f4f4e317dbfa27d3e727a5ba182c0a0
Jared <hurin615@gmail.com>
2018-12-28 12:21:49 -0600

    Fixed grammatical errors

--- src/Game/LevelScripts/CinemaUndergroundTunnel.java
+++ src/Game/LevelScripts/CinemaUndergroundTunnel.java
@@ -17,7 +17,7 @@ public class CinemaUndergroundTunnel extends LevelScript {
     @Override
     public void onTurnEnd() {
         if (playerHasWaterBalloon() && !didMessage){
-            gi.getTextBox().showMessage("You can throw items. Hold " + getInput(InputMap.THROW_ITEM) + " to get ready to throw the weapon you are currently holding, then press " + getInput(InputMap.ATTACK) + " to throw it.<np>You can also throw <cy>any item from your inventory<cw> by pressing " + getInput(InputMap.THROW_ITEM) + " while hovering over the desired item (while you inventory is open).");
+            gi.getTextBox().showMessage("You can throw items. Hold " + getInput(InputMap.THROW_ITEM) + " to get ready to throw the weapon you are currently holding, then press " + getInput(InputMap.ATTACK) + " to throw it.<np>You can also throw <cg>any item from your inventory<cw> by pressing " + getInput(InputMap.THROW_ITEM) + " while hovering over the desired item. (while your inventory is open)");
             didMessage = true;
         }
     }
