793e76c689e3c58e10a951e5f5f2c08ec722acc4
Locke <>
2019-01-14 18:05:53 -0600

    Add start scripts for the Game and for the editor, so that people can click on a file rather than going into the scary command line to launch the editor.

---
+++ CompiledGame/SourceryText-Editor-Linux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Run the level editor
+# There are individual files for this so people don't have to be afraid of the command line
+#
+# This game was built on Java 11, so please use at least that version for running.  You can install java at
+#  https://jdk.java.net/11/
+# If you have multiple java versions on your system, you can change `java` to be the absolute path to the correct
+#  installed version, for example `/usr/lib/jvm/java-11-openjdk-amd64/bin/java`
+
+
+java -jar SourceryText.jar editor
---
+++ CompiledGame/SourceryText-Editor-Windows.bat
@@ -0,0 +1,10 @@
+:: Run the level editor
+:: There are individual files for this so people don't have to be afraid of the command line
+::
+:: This game was built on Java 11, so please use at least that version for running.  You can install java at
+::  https://jdk.java.net/11/
+:: If you have multiple java versions on your system, you can change `javaw` to be the absolute path to the correct
+::  installed version, for example it might be `C:\Program Files\Java\jdk-11\bin\javaw.exe` or it might be somewhere in
+::  your downloads idk
+
+start javaw -jar SourceryText.jar editor
